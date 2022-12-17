fea7b7a9ba79a779d9d789fc980264817079fbff
Danicus2000000 <66948931+Danicus2000000@users.noreply.github.com>
2022-12-17 15:36:40 +0000

    Added activities for each functionality

--- app/src/main/java/bulman/daniel/bleattractions/MainActivity.java
+++ app/src/main/java/bulman/daniel/bleattractions/MainActivity.java
@@ -2,7 +2,9 @@ package bulman.daniel.bleattractions;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openExplore(View view) {//opens explore window
+        Intent openExploreIntent=new Intent(getApplicationContext(),AreaExplore.class);
+        startActivity(openExploreIntent);
+    }
+
+    public void openTicketManager(View view) {//opens ticket manager
+        Intent openTicketManagerIntent=new Intent(getApplicationContext(),TicketManager.class);
+        startActivity(openTicketManagerIntent);
+    }
+
+    public void openQueueExplorer(View view) {//opens queue explorer
+        Intent openQueueManagerIntent=new Intent(getApplicationContext(),QueueExplorer.class);
+        startActivity(openQueueManagerIntent);
     }
 }
