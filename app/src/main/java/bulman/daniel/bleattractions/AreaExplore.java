917325ab6e08e77d351f447d68ce36432902aa67
Dan <dbulman68@gmail.com>
2022-12-20 04:00:21 +0000

    Allowed bluetooth to be toggled on and off

--- app/src/main/java/bulman/daniel/bleattractions/AreaExplore.java
+++ app/src/main/java/bulman/daniel/bleattractions/AreaExplore.java
@@ -1,7 +1,10 @@
 package bulman.daniel.bleattractions;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -17,14 +20,24 @@ import java.util.ArrayList;
 public class AreaExplore extends AppCompatActivity {
 
     private TextView contentDisplay;
-    private ArrayList<BluetoothDevice> deviceList;
+    private ArrayList<BluetoothDevice> mDeviceList;
+    private BluetoothAdapter mBluetoothAdapter;
+    private boolean mReceiverIsRegistered;
+    private static final int REQUEST_ENABLE_BT=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_area_explore);
         contentDisplay = findViewById(R.id.DeviceList);
-        deviceList = new ArrayList<>();
+        mDeviceList = new ArrayList<>();
+        mReceiverIsRegistered=false;
+        mBluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter==null)
+        {
+            Toast.makeText(this,"Bluetooth is not supported on this device!",Toast.LENGTH_SHORT).show();
+            finish();
+        }
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
             Toast.makeText(this, "Bluetooth is not supported on this device!", Toast.LENGTH_SHORT).show();
             finish();
@@ -33,8 +46,17 @@ public class AreaExplore extends AppCompatActivity {
             Toast.makeText(this, "Bluetooth low energy is not supported on this device!", Toast.LENGTH_SHORT).show();
             finish();
         }
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(receiver, filter);
+        if(!mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBT=new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBT,REQUEST_ENABLE_BT);
+        }
+        else
+        {
+            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(receiver, filter);
+            mReceiverIsRegistered=true;
+        }
     }
 
     // Create a BroadcastReceiver for ACTION_FOUND.
@@ -44,23 +66,44 @@ public class AreaExplore extends AppCompatActivity {
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 // Discovery has found a device. Get the BluetoothDevice
                 // object and its info from the Intent.
-                deviceList.add(intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE));
+                mDeviceList.add(intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE));
                 updateDisplay();
             }
         }
     };
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==REQUEST_ENABLE_BT)
+        {
+            if(resultCode==RESULT_OK)
+            {
+                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                registerReceiver(receiver, filter);
+                mReceiverIsRegistered=true;
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(),"Bluetooth is needed to detect devices!",Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(receiver);
+        if(mReceiverIsRegistered) {
+            unregisterReceiver(receiver);
+        }
     }
 
     private void updateDisplay() {
-        StringBuilder toSet = new StringBuilder();
-        for (int i = 0; i < deviceList.size(); i++) {
-            toSet.append(" MAC: ").append(deviceList.get(i).getAddress()).append("\n");
+        String toSet="";
+        for (int i = 0; i < mDeviceList.size(); i++) {
+            toSet+="Name: "+mDeviceList.get(i).getName()+"MAC Address: "+mDeviceList.get(i).getAddress()+"\n";
         }
-        contentDisplay.setText(toSet.toString());
+        contentDisplay.setText(toSet);
     }
 }
